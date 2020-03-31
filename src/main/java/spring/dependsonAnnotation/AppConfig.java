a6a60b032147fbba420d2c9912d64c4f9730c5cc
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-03-31 18:22:10 +0100

    Create a AppConfig class that creates FirstBean,SecondBean and ThirdBean bean and initialize the FirstBean bean based only when SecondBean and ThirdBean have been initialized.

---
+++ src/main/java/spring/dependsonAnnotation/AppConfig.java
@@ -0,0 +1,28 @@
+package spring.dependsonAnnotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class AppConfig {
+    @Bean("firstBean")
+    @DependsOn(value = {
+            "secondBean",
+            "thirdBean"
+    })
+    public FirstBean firstBean() {
+        return new FirstBean();
+    }
+
+
+    @Bean("secondBean")
+    public SecondBean secondBean() {
+        return new SecondBean();
+    }
+
+    @Bean("thirdBean")
+    public ThirdBean thirdBean() {
+        return new ThirdBean();
+    }
+}
