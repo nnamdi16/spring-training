f5c3b26654f01ae5c60fad830fb0e4091e8e065a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-03-31 08:45:17 +0100

    Create the Java configuration file to scan for spring beans

---
+++ src/main/java/spring/dependencyInjectionJavaConfig/AppConfiguration.java
@@ -0,0 +1,10 @@
+package spring.dependencyInjectionJavaConfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring.dependencyInjectionJavaConfig")
+public class AppConfiguration {
+
+}
