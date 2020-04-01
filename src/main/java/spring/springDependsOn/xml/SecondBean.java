0c0230ab5b5eda8198ca5a4432640dc5a398cf72
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-01 15:19:58 +0100

    Created a SecondBean class that would instantiate before any other bean and be injected into FirstBean by autowiring by type

---
+++ src/main/java/spring/springDependsOn/xml/SecondBean.java
@@ -0,0 +1,13 @@
+package spring.springDependsOn.xml;
+
+public class SecondBean {
+    public SecondBean() {
+        System.out.println("SecondBean Initialized via constructor");
+    }
+
+    public void display() {
+        System.out.println("SecondBean method called");
+    }
+}
+
+
