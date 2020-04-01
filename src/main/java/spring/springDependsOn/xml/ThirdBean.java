7ce39a8bc94808d1185fade890eb5ecac189cf02
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-01 15:21:29 +0100

    Created a ThirdBean class that would instantiate before any after SecondBean bean and before FirstBean bean and be injected into FirstBean by autowiring by type

---
+++ src/main/java/spring/springDependsOn/xml/ThirdBean.java
@@ -0,0 +1,11 @@
+package spring.springDependsOn.xml;
+
+public class ThirdBean {
+    public ThirdBean() {
+        System.out.println("ThirdBean initialized via constructor");
+    }
+
+    public void display() {
+        System.out.println("Third bean method called");
+    }
+}
