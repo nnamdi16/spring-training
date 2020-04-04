e73769ea9eade3acd1780e8bea4cabf4086a084a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-04 13:43:42 +0100

    Created FirstBean and SecondBean class

---
+++ src/main/java/spring/lazyAnnotation/FirstBeans.java
@@ -0,0 +1,12 @@
+package spring.lazyAnnotation;
+
+public class FirstBeans {
+
+    public FirstBeans() {
+        System.out.println("Inside the first bean constructor");
+    }
+
+    public void test() {
+        System.out.println("Method of FirstBean class ");
+    }
+}
---
+++ src/main/java/spring/lazyAnnotation/SecondBeans.java
@@ -0,0 +1,12 @@
+package spring.lazyAnnotation;
+
+public class SecondBeans {
+
+    public SecondBeans() {
+        System.out.println("Inside the second bean constructor");
+    }
+
+    public void test() {
+        System.out.println("Method of SecondBean class ");
+    }
+}
