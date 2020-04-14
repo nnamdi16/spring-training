2e5880e17d5103e9b4d3297644f297bd9f9ab3fc
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-14 07:52:50 +0100

    Created a method initializeEmailConfig and passed it to afterPropertySet method to be executed at the initializing of the bean. Also create a class called Email including its getter and setter methods and it's toString method

---
+++ src/main/java/spring/beanLifeCycle/initializingBean/EmailConfigCache.java
@@ -0,0 +1,68 @@
+package spring.beanLifeCycle.initializingBean;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConfigCache implements InitializingBean {
+
+    public void initializeEmailConfig() {
+        Email email = new Email();
+        email.setSubject("Demo mail");
+        email.setMsg("Demo mail message");
+        email.setTo("flora@gmail.com");
+        email.setFrom("flora@gmail.com");
+        System.out.println(email.toString());
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        initializeEmailConfig();
+    }
+}
+
+
+class Email {
+    private String subject;
+    private String to;
+    private String from;
+    private String msg;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Email [subject=" + subject + ", to=" + to + ", from=" + from + ", msg=" + msg + "]";
+    }
+}
