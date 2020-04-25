2fa5e1aded0dd334fa03e8bdc5a62a40b4dfa90f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-25 22:40:04 +0100

    Added @Configuration and @ComponentScan to scan beans in a package

---
+++ src/main/java/spring/componentScan/AppConfig.java
@@ -0,0 +1,9 @@
+package spring.componentScan;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring.componentScan")
+public class AppConfig {
+}
