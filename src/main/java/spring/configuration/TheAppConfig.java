d5eb6ca45914d9c84d8fb57554208680131df442
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-25 10:57:25 +0100

    Added @PropertySource  annotation to read properties from property file and @Value annotation to read values  inside @Configuration classes

--- src/main/java/spring/configuration/TheAppConfig.java
+++ src/main/java/spring/configuration/TheAppConfig.java
@@ -1,12 +1,20 @@
 package spring.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:property/myproject.properties")
 public class TheAppConfig {
+    @Value("${cp.user.country}")
+    private String country;
+
+    @Value("${cp.user.role}")
+    private String role;
     /**
      * Injecting externalized values and other beans using @Autowired
      * inside @Configuration classes
@@ -17,6 +25,8 @@ public class TheAppConfig {
 
     @Bean
     public Blogger theUser() {
+        System.out.println("Country: " + country);
+        System.out.println("Role: " + role);
         System.out.println(env.getProperty("java.home") + " Thinking loud");
         return new Blogger("travis124");
     }
