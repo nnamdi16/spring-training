24e1c46f0316f4308032e4e8faa138308b84b1be
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-25 14:03:07 +0100

    Added @Scheduled annotation to find the class with a scheduled task

--- src/main/java/spring/configuration/Task.java
+++ src/main/java/spring/configuration/Task.java
@@ -3,7 +3,7 @@ package spring.configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class Task {
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedDelay = 10000)
     public void doTask() {
         System.out.println("Work is in Progress.........");
     }
