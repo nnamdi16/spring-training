3431e53a00546dec3f32e85c29798724e5a98cea
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-28 00:17:28 +0100

    Create notes with respect to having more than one profiles using profile attributes.

--- src/main/java/spring/profileAnnotation/profileAndBeanAnnotation/Animals.java
+++ src/main/java/spring/profileAnnotation/profileAndBeanAnnotation/Animals.java
@@ -3,3 +3,24 @@ package spring.profileAnnotation.profileAndBeanAnnotation;
 public interface Animals {
     String getMessage();
 }
+/**
+ * Define more than one profiles in @Component and @Configuration
+ * using profile attributes
+ *
+ * @Configuration
+ * @Profile({"dev", "test"})
+ * public class DevEnvConfig {
+ * ------
+ * }
+ * For XML
+ * <beans profile="dev, test" >
+ * <p>
+ * Case-3: Use (!) with profile.
+ * @Configuration
+ * @Profile({"dev", "!prod"})
+ * public class DevEnvConfig {
+ * ------
+ * } For XML
+ * <beans profile="dev, !prod" >
+ * The above config file will be registered if either dev or any other profile except prod such as test is activated.
+ */
