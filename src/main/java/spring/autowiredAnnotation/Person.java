bcd1fb4d85180cced7c78710ad7d2eda87bb4c91
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-28 01:41:18 +0100

    Implemented a Person class with @Component to scan the class a bean

---
+++ src/main/java/spring/autowiredAnnotation/Person.java
@@ -0,0 +1,47 @@
+package spring.autowiredAnnotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name;
+    private String address;
+    private int age;
+
+    public Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
