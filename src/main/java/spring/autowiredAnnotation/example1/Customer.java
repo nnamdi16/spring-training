092777ca51be13b9ef33f5a9963eb2025511f033
Nnamdi <nnwabuokei@paga.com>
2020-08-23 22:57:09 +0100

    Added a constructor

--- src/main/java/spring/autowiredAnnotation/example1/Customer.java
+++ src/main/java/spring/autowiredAnnotation/example1/Customer.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
+    public Customer() {
+
+    }
+
     //Field injection;
 //    @Autowired
     private Person person;
