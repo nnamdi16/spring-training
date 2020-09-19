201c43d9f2dda9c3ae4f9fd0ce6c94a5feca63c9
Nnamdi <nnwabuokei@paga.com>
2020-09-19 14:12:40 +0100

    Refactored DatabaseEngineeringService adding more methods

--- src/main/java/spring/inversionOfControl/javaAnnotation/DatabaseEngineeringService.java
+++ src/main/java/spring/inversionOfControl/javaAnnotation/DatabaseEngineeringService.java
@@ -3,11 +3,29 @@ package spring.inversionOfControl.javaAnnotation;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 //@Qualifier("databaseEngineeringService")
 public class DatabaseEngineeringService implements EngineeringService {
+
+    //create an array of strings
+    private String[] data = {
+            "Beware of the wolf's in Sheep's clothing",
+            "Diligence is the mother of good luck",
+            "The journey is the reward"
+    };
+
+    //Create a random number generator
+    private Random random = new Random();
+
     @Override
     public String getEngineeringService() {
-        return "Database Engineering Service";
+
+        //Pick a random string from the array
+        int index = random.nextInt(data.length);
+        String theServiceWords = data[index];
+
+        return "Database Engineering Service: " + theServiceWords ;
     }
 }
